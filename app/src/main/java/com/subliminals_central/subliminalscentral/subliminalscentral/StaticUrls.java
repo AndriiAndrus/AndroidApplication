a53e0a6a2bf39947eed3e4547a56641b0e967302
Rifes0ne <Rifes96@gmail.com>
2015-11-30 22:20:42 +0200

    New file for static urls

---
+++ app/src/main/java/com/subliminals_central/subliminalscentral/subliminalscentral/StaticUrls.java
@@ -0,0 +1,9 @@
+package com.subliminals_central.subliminalscentral.subliminalscentral;
+
+public class StaticUrls {
+
+    public static final String WEBSITE_URL = "";
+    public static final String SHOP_URL = "";
+    public static final String FORUM_URL = "";
+
+}
